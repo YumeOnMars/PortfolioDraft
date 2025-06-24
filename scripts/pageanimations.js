9c0d3be2aaa06f100ee77eee8652563e2365b1fa
YumeOnMars <bryanwatermelon1@gmail.com>
2025-06-23 22:05:36 -0400

    FIXED page transitions. SMOOTH SWIPE with swup now. synced contact form with email

--- scripts/pageanimations.js
+++ scripts/pageanimations.js
@@ -1,3 +1,12 @@
-  const swup = new Swup({
-    plugins: [new SwupHeadPlugin()]
-  });
+const swup = new Swup({
+  plugins: [new SwupHeadPlugin()]
+});
+
+//To Do: 
+//Create Projects Page (zoom wheel)
+// Replace Skills with Connect and put skills like coding languages in porjects
+// Create Passions Page
+// Spotify Track Player
+// Hamburger Menu with the player inside of it
+// possibly make the spotify track player a widget
+// make clean transition across pages + the reversal of it.
