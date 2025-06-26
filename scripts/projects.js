233592c2aeb5a96aed89f428f65ad36eb1945cfc
YumeOnMars <bryanwatermelon1@gmail.com>
2025-06-26 15:48:06 -0400

    added a dropdown on the projects pageusing javascript.

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,10 +1,54 @@
-let revealButton = document.querySelector("#RevealProjectsBtn");
-let backgroundVid = document.querySelector("#ProjectStartBackground");
-let display = document.querySelector("#ProjectDisplayingBackground");
+let dropDownList = document.querySelector(".dropDownList");
 
+let dropDownBtn = document.querySelector(".dropDownBtn");
 
-revealButton.addEventListener("click", function() {
-    backgroundVid.style.display = "none";
-    display.style.display = "block";
-    revealButton.style.display = "none";
+let triangle = document.getElementById("triangle")
+let triangleOpen = false;
+
+dropDownBtn.addEventListener("click", function () {
+    if (triangleOpen === false) {
+        triangle.style.rotate = "-90deg";
+        triangle.style.transition = "ease .6s";
+        triangleOpen = true;
+        let button1 = document.getElementById("l1").animate([
+            {
+                transform: "translateY(-50%)"
+            },
+            {
+                transform: "translateY(-12%)"
+            },
+        ], {
+            duration: 400,
+            fill: "forwards"
+        });
+
+                let button2 = document.getElementById("l2").animate([
+            {
+                transform: "translateY(-160%)"
+            },
+            {
+                transform: "translateY(-0%)"
+            },
+        ], {
+            duration: 400,
+            fill: "forwards"
+        });
+
+                let button3 = document.getElementById("l3").animate([
+            {
+                transform: "translateY(-250%)"
+            },
+            {
+                transform: "translateY(5%)"
+            },
+        ], {
+            duration: 400,
+            fill: "forwards"
+        });
+
+    } else { //if triangle open is open aka down, list shows
+        triangle.style.rotate = "0deg";
+        triangle.style.transition = "ease .6s";
+        triangleOpen = false;
+    }
 })
