1e1393412dff2075482834c6cd022c8435451fed
YumeOnMars <bryanwatermelon1@gmail.com>
2025-07-02 09:47:56 -0400

    done

--- scripts/pageanimations.js
+++ scripts/pageanimations.js
@@ -10,13 +10,12 @@ document.addEventListener('DOMContentLoaded', () => {
     ]
   });
 
-  // Handle transition start
+  
   swup.hooks.on('animation:out:start', () => {
     const ui = document.querySelector('.persistent-ui');
     if (ui) ui.classList.remove('fade-in');
   });
 
-  // Handle content replacement and fade in persistent UI again
   swup.hooks.on('animation:in:end', () => {
     // Allow time for styles/layout to stabilize
     requestAnimationFrame(() => {
