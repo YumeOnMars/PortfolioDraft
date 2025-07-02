b0d9d405a31aaf5793d4493aed00a6596a489caf
YumeOnMars <bryanwatermelon1@gmail.com>
2025-07-02 00:12:02 -0400

    remove flickering

--- scripts/pageanimations.js
+++ scripts/pageanimations.js
@@ -1,9 +1,5 @@
 const swup = new Swup({
-  plugins: [new SwupHeadPlugin()]
+  plugins: [
+    new SwupHeadPlugin(),
+  ]
 });
-
-//To Do: 
-//rescale everything to fit laptop.
-// Add hamburger menu
-// Do Swup transitions
-//figure out music when clicking explore
